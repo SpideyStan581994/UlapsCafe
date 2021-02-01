95bdf1fe2f82a1d299e666f8cca6295836017552
SpideyStan581994 <steven.tan@ulaps.dev>
2021-02-01 14:47:42 +0800

    Added Model and Setup Database in application properties using H2-console Database

---
+++ src/main/java/com/ulap/demo/UlapsCafeApplication.java
@@ -0,0 +1,13 @@
+package com.ulap.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UlapsCafeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UlapsCafeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ulap/demo/UlapsCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ulap.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UlapsCafeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
