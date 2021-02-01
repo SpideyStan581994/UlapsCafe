e6529411c80be2bf710b28371c822999d5627f2c
SpideyStan581994 <steven.tan@ulaps.dev>
2021-02-01 15:28:40 +0800

    Added PostMapping Controller, addCoffee.html.

--- src/main/java/com/ulap/demo/controller/IndexController.java
+++ src/main/java/com/ulap/demo/controller/IndexController.java
@@ -1,7 +1,11 @@
 package com.ulap.demo.controller;
 
+import com.ulap.demo.model.Coffee;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,5 +15,19 @@ public class IndexController {
     @GetMapping
     public String showIndex(){
         return "index";
+    }
+
+    @GetMapping("/addCoffee")
+    public String addCoffee(Model model){
+        Coffee myCoffee = new Coffee();
+        model.addAttribute("coffee",myCoffee);
+        return "addCoffee";
+    }
+
+
+    @PostMapping("/addNewCoffee")
+    public String addNewCoffee(@ModelAttribute Coffee newCoffee){
+        System.out.println(newCoffee);
+        return "redirect:/index";
     }
 }
--- src/main/java/com/ulap/demo/model/Coffee.java
+++ src/main/java/com/ulap/demo/model/Coffee.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 
 @Data
 @Entity
-public class Cafe {
+public class Coffee {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
